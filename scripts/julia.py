0493b459f3bdfc890a8600fb40a3662e1d59eaa3
Tait-A <s2051131@ed.ac.uk>
2025-05-08 13:36:35 +0100

    adding colour to args

---
+++ scripts/julia.py
@@ -0,0 +1,3 @@
+from src.visualiser import julia
+
+julia()
--- src/visualiser.py
+++ src/visualiser.py
@@ -1,7 +1,8 @@
 import numpy as np
 from PIL import Image
-from sets import Fractal, Mandelbrot, Julia
-from colourmap import ColourMap, Colour
+from src.sets import Fractal, Mandelbrot, Julia
+from typing import List, Optional
+from src.colourmap import ColourMap, Colour
 import argparse
 import random
 
@@ -23,6 +24,80 @@ class Visualiser:
 
     def set_smoothing(self, smoothing: bool):
         self.smoothing = smoothing
+
+
+
+def parse(julia: bool = False) -> tuple[Optional[float], Optional[float], ColourMap]:
+    parser = argparse.ArgumentParser(description="Visualise fractals.")
+
+    if julia:
+        parser.add_argument(
+            "--real", type=float, required=False, help="Real part of the complex number. [-1,1]"
+        )
+        parser.add_argument(
+            "--imag", type=float, required=False, help="Imaginary part of the complex number. [-1,1]"
+        )
+
+    parser.add_argument(
+        "--colours", help="Select the colours you use interactively.", action="store_true"
+    )
+
+    args = parser.parse_args()
+
+    if julia:
+        if args.real is not None:
+            real = args.real
+        else:
+            real = random.uniform(-1, 1)
+        if args.imag is not None:
+            imaginary = args.imag
+        else:
+            imaginary = random.uniform(-1, 1)
+    else:
+        real = None
+        imaginary = None
+
+    if args.colours:
+        colours = get_colours()
+    else:
+        no_colours = random.randint(2, 4)
+        colours = [random.choice(list(Colour)) for _ in range(no_colours)]
+
+    colourmap = ColourMap(colours, "rgb")
+    print(f"Using colours: {[colour.name for colour in colours]}")
+    if julia:
+        print(f"Real part: {real}, Imaginary part: {imaginary}")
+
+    return real, imaginary, colourmap
+
+
+def get_colours() -> List[Colour]:
+    """Get a list of colours from the user."""
+    colours = []
+    print("Enter the colours you want to use (e.g. 'red', 'green', 'blue'). \nType 'done' when finished. \nType 'help' for a list of available colours.")
+    while True:
+        colour = input("Colour: ")
+        if colour.lower() == "done":
+            break
+        if colour.lower() == "help":
+            print("Available colours: ", [colour.name for colour in Colour])
+            continue
+        try:
+            colours.append(Colour[colour.upper()])
+        except KeyError:
+            print(f"'{colour}' is not a valid colour. Please try again.")
+    return colours
+
+
+def julia() -> None:
+    """Visualise a Julia set."""
+    real, imaginary, colourmap = parse(julia=True)
+
+    c = complex(real, imaginary)
+    j = Julia(c, 100, 1000)
+    v = Visualiser(j, smoothing=True, colourmap=colourmap)
+
+    v.display()
 
 
 if __name__ == "__main__":
